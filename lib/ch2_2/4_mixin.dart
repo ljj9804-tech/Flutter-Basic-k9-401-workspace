371d899be47fac5373c69a9b557b71d4e6b44d51
lsy3709 <lsy3709@naver.com>
2026-03-30 11:43:57 +0900

    3교시, 클래스 샘플코드10, 5_abstract_class

--- lib/ch2_2/4_mixin.dart
+++ lib/ch2_2/4_mixin.dart
@@ -21,8 +21,14 @@ mixin IdolSingMixin on Idol{
   }
 }
 
+mixin IdolDanceMixin on Idol{
+  void dance(){
+    print('${this.name}이 춤을 춥니다!.');
+  }
+}
+
 // 믹스인을 적용할 때는 with 키워드 사용
-class BoyGroup extends Idol with IdolSingMixin{
+class BoyGroup extends Idol with IdolSingMixin,IdolDanceMixin {
   BoyGroup(
       super.name,
       super.membersCount,
@@ -33,9 +39,38 @@ class BoyGroup extends Idol with IdolSingMixin{
   }
 }
 
+class GirlGroup extends Idol with IdolSingMixin,IdolDanceMixin{
+  final String name;
+  final int membersCount;
+
+  GirlGroup(
+      this.name,
+      this.membersCount,
+      ) : super(   // super는 부모 클래스를 지칭합니다.
+  name,
+  membersCount,
+  );
+
+  void sayName() {
+    print('저는 여자 아이돌 ${this.name}입니다.');
+  }
+
+  void sayMembersCount() {
+    print('${this.name} 멤버는 ${this.membersCount}명입니다.');
+  }
+}
+
 void main(){
   BoyGroup bts = BoyGroup('BTS', 7);
 
   // 믹스인에 정의된 dance() 함수와 sing() 함수 사용 가능
   bts.sing();
+  bts.dance();
+
+  GirlGroup redVelvet = GirlGroup('레드벨벳', 5);
+
+  redVelvet.sayName();
+  redVelvet.sayMembersCount();
+  bts.sing();
+  bts.dance();
 }
