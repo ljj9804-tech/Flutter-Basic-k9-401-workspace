7e45d354089b87e9d7b530f451c6c2c0e7bde10c
lsy3709 <lsy3709@naver.com>
2026-04-01 11:14:37 +0900

    3교시,기본 위젯 확인3

--- lib/screen/basic1/_8_MyAppClass.dart
+++ lib/screen/basic1/_8_MyAppClass.dart
@@ -202,6 +202,20 @@ class MyAppTest extends StatelessWidget {
                       child: Container(color: Colors.red, width: 50.0, height: 50.0),
                     ),
                   ),
+                ),
+                SizedBox(
+                  height: 400,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(height: 50.0, width: 50.0, color: Colors.red),
+                      SizedBox(width: 12.0),
+                      Container(height: 50.0, width: 50.0, color: Colors.green),
+                      SizedBox(width: 12.0),
+                      Container(height: 50.0, width: 50.0, color: Colors.blue),
+                    ],
+                  ),
                 )
               ],
             ),
